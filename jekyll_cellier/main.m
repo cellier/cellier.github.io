6a2c9714a1cc83c9f843df33eb1a1f43ae742c5b
cellier <shengbixing@gmail.com>
2015-01-01 10:23:06 +0800

    第0次更新
    第0次更新

---
+++ jekyll_cellier/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  jekyll_cellier
+//
+//  Created by cellier on 14/12/31.
+//  Copyright (c) 2014年 cellier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
